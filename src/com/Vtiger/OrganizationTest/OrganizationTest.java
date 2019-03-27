6882d8211a6845cd21e0310a95abe57eaa30643c
prashanthsgouda <prashant.gouda@gmail.com>
2019-03-27 16:56:55 +0530

    Update OrganizationTest.java

--- src/com/Vtiger/OrganizationTest/OrganizationTest.java
+++ src/com/Vtiger/OrganizationTest/OrganizationTest.java
@@ -17,6 +17,7 @@ public class OrganizationTest extends BaseClass{
 	public void createOrganizationTest() throws Throwable{
 		
 		//Read data For Prashanth Test
+		//Because Git Demo
 		String organizationName = fLib.getExcelData("Sheet1", 1, 2);
 		//Click on Organization link
 		HomePage hp = PageFactory.initElements(driver, HomePage.class);
